80baec937488d95b0dc257933157085ec16f792f
Brannon <57905614+BrannonOpperman@users.noreply.github.com>
2019-12-11 22:43:14 +0200

    Created components and Navbar and Installed React-Router and Added Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home'
+import about from './components/About'
+import contact from './components/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Poke'times</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={about} />
+        <Route path='/contact' component={contact} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error architecto perspiciatis ipsum itaque placeat, saepe, eius sequi beatae exercitationem ad facere earum unde cupiditate quas. Maxime, est rerum. Veritatis, nesciunt!</p>
+        </div>
+    )
+}
+
+export default About
